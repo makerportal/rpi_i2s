d0ca1676509609161e1e3600ae6bbb684b14959b
Joshua Hrisko <49917891+josh-hrisko@users.noreply.github.com>
2020-11-22 20:47:59 -0500

    Update i2s_dev_checker.py

--- i2s_dev_checker.py
+++ i2s_dev_checker.py
@@ -7,4 +7,7 @@ import pyaudio
 audio = pyaudio.PyAudio() # start pyaudio
 for ii in range(0,audio.get_device_count()):
     # print out device info
-    print(audio.get_device_info_by_index(ii)) 
+    dev = audio.get_device_info_by_index(ii) # device name
+    print('Device Name: {}'.format(dev['name']))
+    for param in list(dev.keys()):
+        print('\t {0}: {1}'.format(param,dev[param])) # device parameter info
